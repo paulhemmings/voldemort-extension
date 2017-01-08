882449637de3fe398663d3ec4652cb1f07318587
paul hemmings <phemmings@tacitknowledge.com>
2017-01-08 08:02:01 -0800

    monitor for newly added dynamic content

--- content.js
+++ content.js
@@ -1,21 +1,27 @@
-var elements = document.getElementsByTagName('*');
+(function() {
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+    function replaceText() {
+        var elements = document.getElementsByTagName('*');
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+            for (var j = 0; j < element.childNodes.length; j++) {
+                var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text
-                                .replace(/Donald Trump|Donald J. Trump/gi, 'Voldemort')
-                                .replace(/Melania Trump/gi, 'Wife of Voldemort')
-                                .replace(/Trump/gi, 'Voldemort')
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var replacedText = text
+                                        .replace(/Donald Trump|Donald J. Trump/gi, 'Voldemort')
+                                        .replace(/Melania Trump/gi, 'Wife of Voldemort')
+                                        .replace(/Trump/gi, 'Voldemort')
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
+                    }
+                }
             }
         }
     }
-}
+    setInterval(replaceText, 3000);
+
+})();
